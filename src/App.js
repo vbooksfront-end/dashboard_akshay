b2c8c033c981e4c9e617750ae9fb69386a4fb821
frontend@vbookshr.com <frontend@vbookshr.com>
2023-10-13 18:45:22 +0530

    dummy_commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <center> <h1>WELCOME to DUMMY REACT</h1></center>
+    </div>
+  );
+}
+
+export default App;
